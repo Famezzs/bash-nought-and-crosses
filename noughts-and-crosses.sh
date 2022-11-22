5d1335e6e9583b1f4481f4c16d83a57a884ecde0
Famezzs <Andrii.Kozubenko.IT.2021@lpnu.ua>
2022-11-22 20:20:42 +0200

    Start adding comments

--- noughts-and-crosses.sh
+++ noughts-and-crosses.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Declaration of a game board utilizing associative array for matrix simulation
+# Оголошення ігрового поля як асоціативного масиву для імітації матриці
 declare -A gameBoard
 boardSize=3
 movesPerformed=0
@@ -10,11 +12,19 @@ playerSignature="x"
 computerSignature="o"
 blankSignature=" "
 
+# Announce current round by calculating it as the amount of all moves performed by players 
+# int divided by 2 add increased by 1
+# Оголошення теперішнього раунду, порядок якого вираховується шляхом цілочисельного ділення 
+# загальної кількості кроків на 2 та додавання 1
 announceRound() {
 	clear
 	echo -en "Round #$((movesPerformed/2+1))\n\n"
 }
 
+# Announce winner of the game by using first argument provided during invocation as the name of winner,
+# print game board, and declare game over by assigning isGameOver true
+# Оголосити переможця, використовуючи перший наданий аргумент як ім'я переможця, вивести ігрове поле
+# та оголосити кінець гри присвоєнням isGameOver true
 announceWinner() {
 	clear
 	echo "$1 is the winner!"
@@ -23,6 +33,8 @@ announceWinner() {
 	exit 0
 }
 
+# Announce draw, print the game board, and declare game over by assigning isGameOver true
+# Оголосити нічию, вивести ігрове поле та оголосити кінець гри присвоєнням isGameOver true
 announceDraw() {
 	clear
 	echo -en "\nIt's a draw!\n\n"
@@ -31,6 +43,8 @@ announceDraw() {
 	exit 0
 }
 
+# Print game board and example of how to refer to game field's cells
+# Виведення ігрового поля з прикладом того, як вказати певне місце на ньому
 printBoard() {
 	echo "Example:            Board: (${playerSignature} - you, ${computerSignature} - computer)"
 	echo "-------------       -------------"
@@ -42,6 +56,8 @@ printBoard() {
 	echo "-------------       -------------"
 }
 
+# Fill the game board's cells with empty values to signify them not being marked by any of the players
+# Заповнення клітинок ігрового поля пустим значенням як знак того, що ви не заняті жодним із гравців
 initializeBoard() {
 	for ((row=0;row<boardSize;++row))
 	do
@@ -52,7 +68,11 @@ initializeBoard() {
 	done
 }
 
+# Try to finish player's move by using the position on the board provided as first argument
+# Спробувати завершити хід гравця користуючись номером позиції, що передана як перший аргумент функції
 tryPerformPlayerMove() {
+	# Check whether move position provided as first argument is withing range of 1 to 9 inclusive
+	# Перевірка, чи позиція задана першим аргументом функції належить до проміжку від 1 до 9 включно	
 	[ $((9-$1)) -le 8 ] && [ $((9-$1)) -ge 0 ] && positionIsValid=true || positionIsValid=false
 
 	if ! $positionIsValid
@@ -61,6 +81,8 @@ tryPerformPlayerMove() {
 		return
 	fi
 
+	# Convert int move position provided to corresponding indexes in gameBoard matrix
+	# Конвертація наданої позиції кроку в індекси gameBoard матриці
 	convertMovePositionToIndexes "$1"
 
 	if [ "${gameBoard[${moveRow},${moveColumn}]}" != "${blankSignature}" ]
@@ -73,6 +95,10 @@ tryPerformPlayerMove() {
 	movePerformedSuccessfully=true
 }
 
+# Works exactly like tryPerformPlayerMove() but without a check whether or not the move position provided is in range of 1 to 9 inclusive
+# Such check is ommitted due to the fact that computer already generates movePosition values in this range
+# Працює ідентично до tryPerformPlayerMove() за винятком відсутності перевірки на належність позиції кроку до інтервалу від 1 до 9 включно
+# Цю перевірку недоцільно виконувати, адже програма і так генерує значення movePosition у цьому проміжку 
 tryPerformComputerMove() {
 	convertMovePositionToIndexes "$1"
 
